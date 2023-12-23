7d48190a58b2a0e8e53599ad789bbc41f2d91500
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-23 22:45:10 +0600

    eror

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../Layout/RootLayout";
 import Home from "../Pages/Home/Home";
+import Error from "../components/Error";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
+        errorElement: <Error />,
         children: ([
             {
                 path: "/",

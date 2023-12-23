44cef75f838e63bebf57067136e10afb51f73e6c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-24 00:37:58 +0600

    nav stop

--- src/Log/Banner.jsx
+++ src/Log/Banner.jsx
@@ -4,10 +4,10 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
+
+import ban1 from "../assets/banner/ban1.jpg"
 
 const Banner = () => {
     return (
@@ -28,7 +28,7 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide><img src={ban1} alt="" /></SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>

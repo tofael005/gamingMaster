9d4e910ae12474d8d5ec18fa969f8801455d989c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-30 00:04:29 +0600

    nav us

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
                 </div>
                 <ul className="flex items-center gap-7">
                     <Link to="/"><li>Home</li></Link>
-                    <Link to="/about"><li>About us</li></Link>
+                    <Link to="/about"><li>About Us</li></Link>
                     <Link to="/portfolio"><li>Portfolio</li></Link>
                     <Link to="/news"><li>News</li></Link>
                     <Link to="/contact"><input className="cursor-pointer hover:bg-blue-500 duration-300 hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded" type="button" value="Contact" /></Link>

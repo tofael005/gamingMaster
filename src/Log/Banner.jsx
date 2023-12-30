ded026588225a27c84b1c8263dc930119adb1bc5
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-30 21:47:57 +0600

    slider edt

--- src/Log/Banner.jsx
+++ src/Log/Banner.jsx
@@ -10,11 +10,12 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import ban1 from "../assets/banner/ban1.jpg"
 import ban2 from "../assets/banner/ban2.jpg"
 import ban3 from "../assets/banner/ban3.jpg"
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
-    return (
-        <div>
-            <Swiper
+  return (
+    <div>
+      <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -28,12 +29,54 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper h-[85vh]"
       >
-        <SwiperSlide><img className='object-cover md:h-full md:w-full' src={ban1} alt="" /></SwiperSlide>
-        <SwiperSlide><img className='object-cover md:h-full md:w-full' src={ban2} alt="" /></SwiperSlide>
-        <SwiperSlide><img className='object-cover md:h-full md:w-full' src={ban3} alt="" /></SwiperSlide>
+        <SwiperSlide>
+
+          <div>
+            <img className='object-cover relative md:h-full md:w-full' src={ban1} alt="" />
+            <div className='absolute text-white px-3 left-0 top-0 text-center w-full h-full
+             bg-black/60 backdrop-brightness-100"'>
+              <p className='md:mt-36 mt-4 text-red-400 font-semibold'>Proved By prodesigner</p>
+              <h1 className='md:text-5xl font-bold md:my-5 text-2xl'>Work that we produce for our clients</h1>
+              <p className='text-transparent md:text-white'>Play the game, learn, thrive, and repeat. Be calm, be a gamer, be a game lover. Life is too short to play a bad game. The future is a game.</p>
+              <Link to="/about">
+                <input className='cursor-pointer absolute top-[70px] right-[95px] md:right-[626px] md:top-[300px] hover:bg-blue-500 mt-12 duration-300 md:text-xl hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded' type="button" value="Get more details" />
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img className='object-cover relative md:h-full md:w-full' src={ban2} alt="" />
+            <div className='absolute text-white px-3 left-0 top-0 text-center w-full h-full
+             bg-black/60 backdrop-brightness-100"'>
+              <p className='md:mt-36 mt-4 text-red-400 font-semibold'>Proved By prodesigner</p>
+              <h1 className='md:text-5xl font-bold md:my-5 text-2xl'>Forge Your Destiny, Conquer the Challenge</h1>
+              <p className='text-transparent md:text-white'>Play the game, learn, thrive, and repeat. Be calm, be a gamer, be a game lover. Life is too short to play a bad game. The future is a game.</p>
+              <Link to="/about">
+                <input className='cursor-pointer absolute top-[70px] right-[95px] md:right-[626px] md:top-[300px] hover:bg-blue-500 mt-12 duration-300 md:text-xl hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded' type="button" value="Get more details" />
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img className='object-cover relative md:h-full md:w-full' src={ban3} alt="" />
+            <div className='absolute text-white px-3 left-0 top-0 text-center w-full h-full
+             bg-black/60 backdrop-brightness-100"'>
+              <p className='md:mt-36 mt-4 text-red-400 font-semibold'>Proved By prodesigner</p>
+              <h1 className='md:text-5xl font-bold md:my-5 text-2xl'>Gaming Redefined, Adventure Refined</h1>
+              <p className='text-transparent md:text-white'>Play the game, learn, thrive, and repeat. Be calm, be a gamer, be a game lover. Life is too short to play a bad game. The future is a game.</p>
+              <Link to="/about">
+                <input className='cursor-pointer absolute top-[70px] right-[95px] md:right-[626px] md:top-[300px] hover:bg-blue-500 mt-12 duration-300 md:text-xl hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded' type="button" value="Get more details" />
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -7,25 +7,27 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const NavBar = () => {
     const [toggle, setToggle] = useState()
     return (
-        <div className="px-3">
+        <div className="px-3 md:px-0 max-w-[1240px] mx-auto">
             <div className="flex justify-between items-center py-4">
                 <div>
                     <Link to="/"><img className="w-[70px]" src={logo} alt="" /></Link>
                 </div>
 
-                <span>
+                <span className="md:hidden">
                     {
-                        toggle ? <IoMdClose  onClick={() => setToggle(!toggle)}/> : <GiHamburgerMenu onClick={() => setToggle(!toggle)}/>
+                        toggle ? <IoMdClose className="text-4xl bg-red-400 rounded-md" onClick={() => setToggle(!toggle)} /> : <GiHamburgerMenu className="text-4xl" onClick={() => setToggle(!toggle)} />
                     }
                 </span>
 
-                <ul className={`flex md:w-[800px] items-center duration-300 flex-col py-4 md:p-0 text-white md:text-gray-900 bg-opacity-100 top-20 bg-black md:bg-transparent w-full text-center md:flex-row gap-4 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/about"><li>About Us</li></Link>
-                    <Link to="/portfolio"><li>Portfolio</li></Link>
-                    <Link to="/news"><li>News</li></Link>
-                    <Link to="/contact"><input className="cursor-pointer hover:bg-blue-500 duration-300 hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded" type="button" value="Contact" /></Link>
-                </ul>
+                <div className={`flex items-center duration-300 flex-col py-5 md:p-0 text-white md:text-gray-300 bg-opacity-100 top-[100px] bg-black md:bg-transparent w-full text-center md:w-auto md:flex-row gap-6 absolute md:static z-50 ${toggle ? "left-0" : "-left-full"}`}>
+                    <ul className="flex flex-col md:flex-row gap-6 font-semibold items-center">
+                        <Link to="/"><li>Home</li></Link>
+                        <Link to="/about"><li>About Us</li></Link>
+                        <Link to="/portfolio"><li>Portfolio</li></Link>
+                        <Link to="/news"><li>News</li></Link>
+                        <Link to="/contact"><input className="cursor-pointer hover:bg-blue-500 duration-300 hover:text-red-400 font-semibold bg-red-400 px-7 py-2 rounded" type="button" value="Contact" /></Link>
+                    </ul>
+                </div>
             </div>
         </div>
     );

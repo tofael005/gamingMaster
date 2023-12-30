805665715e360b1034458f92672a8f284dcd9179
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-30 18:24:42 +0600

    react icon is

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/icon/logo.png"
+import { useState } from "react";
 
 const NavBar = () => {
+    const [toggle, setToggle] = useState()
     return (
         <div className="px-3">
             <div className="flex justify-between items-center py-4">
                 <div>
                     <Link to="/"><img className="w-[70px]" src={logo} alt="" /></Link>
                 </div>
+
+                <span>
+                    {
+                        
+                    }
+                </span>
+
                 <ul className="flex items-center gap-7">
                     <Link to="/"><li>Home</li></Link>
                     <Link to="/about"><li>About Us</li></Link>

d21f9466274b0ca259855de6747d5c1ada4d2311
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-01 00:37:49 +0600

    Stay loop

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../Log/Banner";
 import GmgBanner from "../../components/GmgBnner/GmgBanner";
 import Industry from "../../components/Industry/Industry";
 import OurRecent from "../../components/OurRecentP/OurRecent";
+import StayInLoop from "../../components/StayInTheLoop/StayInLoop";
 import CurrentTending from "../../components/TendingGame/CurrentTending";
 
 
@@ -14,6 +15,7 @@ const Home = () => {
             <Industry />
             <GmgBanner />
             <OurRecent />
+            <StayInLoop />
         </div>
     );
 };

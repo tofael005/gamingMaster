1aaed4dcdf7bc200e3d36888d6e38647c269c88d
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-02 23:31:03 +0600

    about sec

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,20 @@
-
+import about from "../../assets/game/about.png"
+import { IoIosArrowForward } from "react-icons/io";
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="max-w-[1240px] mx-auto px-3 md:px-0 mt-10">
+            <div className="flex flex-col md:flex-row justify-between">
+                <div>
+                    <div className="flex items-center gap-2">
+                        <h1 className="flex items-center gap-2">Home <IoIosArrowForward /></h1>
+                        <p className="">About</p>
+                    </div>
+                </div>
+                <div>
+                    <img className="w-[600px]" src={about} alt="" />
+                </div>
+            </div>
         </div>
     );
 };

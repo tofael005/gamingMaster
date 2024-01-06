97c4b4f7fd489d7016fb4b13a31f008fcd2f19bf
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-07 01:05:07 +0600

    news added

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const News = () => {
     return (
-        <div className="max-w-[1240px] ">
+        <div className="max-w-[1240px] mx-auto md:mt-16 mt-10">
             <div className="flex items-center gap-2">
                         <Link to="/"><h1 className="flex items-center gap-2 cursor-pointer scale-100 hover:scale-95 duration-300">Home <IoIosArrowForward /></h1></Link>
                         <Link to="/about"><p className="text-sm text-red-600 cursor-pointer">About</p></Link>

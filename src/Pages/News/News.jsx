b0d5c0f1ab44fc6acd92c7cddabd051bb9893e9b
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-07 01:05:43 +0600

    news  link

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -7,7 +7,7 @@ const News = () => {
         <div className="max-w-[1240px] mx-auto md:mt-16 mt-10">
             <div className="flex items-center gap-2">
                         <Link to="/"><h1 className="flex items-center gap-2 cursor-pointer scale-100 hover:scale-95 duration-300">Home <IoIosArrowForward /></h1></Link>
-                        <Link to="/about"><p className="text-sm text-red-600 cursor-pointer">About</p></Link>
+                        <Link to="/about"><p className="text-sm text-red-600 cursor-pointer">News</p></Link>
                     </div>
         </div>
     );

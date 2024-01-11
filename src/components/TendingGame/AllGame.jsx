00ed65523fd75a4d921cdba53989c0f32656b7e4
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-12 00:46:44 +0600

    allgm

--- src/components/TendingGame/AllGame.jsx
+++ src/components/TendingGame/AllGame.jsx
@@ -2,7 +2,7 @@
 
 const AllGame = () => {
     return (
-        <div>
+        <div className="max-w-[1240px] mx-auto md:mt-20 mt-10 px-3 md:px-0">
             <h1>All Game</h1>
         </div>
     );

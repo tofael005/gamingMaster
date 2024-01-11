a1f121c9c8d0d9f0aeb4121f10eda11d63001f14
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-12 00:49:52 +0600

    gameSingle

--- src/components/TendingGame/SingleGame.jsx
+++ src/components/TendingGame/SingleGame.jsx
@@ -2,7 +2,7 @@
 
 const SingleGame = () => {
     return (
-        <div >
+        <div className="max-w-[1240px] mx-auto md:mt-16 mt-10 px-3 md:px-0">
             <h1>Single Game</h1>
         </div>
     );
